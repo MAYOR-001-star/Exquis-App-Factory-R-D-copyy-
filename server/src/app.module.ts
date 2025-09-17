97de836ee45635d29d43e3f9e6e8339e2ac4fc50
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-17 04:34:45 +0100

    added the whole files like userSchema, dto(validation), just waiting for mongodb

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './core/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
