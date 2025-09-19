f0cfbc9c0e31f670418db4ba491899da9f7e3e7f
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-19 18:25:43 +0100

    updated main.ts for validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,20 @@
-import { Logger } from '@nestjs/common';
-
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { bootstrapApplication } from './app';
 
 async function bootstrap() {
-	const PORT = 3110;
-	const application = await bootstrapApplication();
-	await application.listen(PORT);
-	Logger.debug(`Server running on ${await application.getUrl()}/api`);
+  const PORT = 3110;
+  const app = await bootstrapApplication();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  await app.listen(PORT);
+  Logger.debug(`Server running on ${await app.getUrl()}/api`);
 }
 bootstrap();
