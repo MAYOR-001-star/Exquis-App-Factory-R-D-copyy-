fb04bbb05d4d7fdc20ebc757863946c07fcd8f92
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-19 18:40:09 +0100

    connected the mongo db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './core/auth/auth.module';
-import { AuthModule } from './auth/auth.module';
+
+const url =
+  'mongodb+srv://exquis_user:GL96sl6ishfSb75u@auth-gateway-db.zfwyaux.mongodb.net/?retryWrites=true&w=majority&appName=auth-gateway-db';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot(url),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
