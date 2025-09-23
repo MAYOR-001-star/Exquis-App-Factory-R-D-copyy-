b1a7e3ce3024afac35a83b1b77ce4be411cc6158
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-23 11:33:45 +0100

    added swagger docs

--- server/src/core/auth/dto/auth.dto.ts
+++ server/src/core/auth/dto/auth.dto.ts
@@ -1,21 +1,27 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterUserDto {
+  @ApiProperty({ example: 'mayor', description: 'Unique username' })
   @IsNotEmpty({ message: 'Username is required' })
   username: string;
 
+  @ApiProperty({ example: 'mayor@example.com', description: 'Valid email address' })
   @IsEmail({}, { message: 'Email must be valid' })
   email: string;
 
+  @ApiProperty({ example: 'strongPassword123', description: 'At least 6 characters' })
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(6, { message: 'Password must be at least 6 characters' })
   password: string;
 }
 
 export class LoginUserDto {
+  @ApiProperty({ example: 'mayor@example.com', description: 'Valid email address' })
   @IsEmail({}, { message: 'Email must be valid' })
   email: string;
 
+  @ApiProperty({ example: 'strongPassword123', description: 'At least 6 characters' })
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(6, { message: 'Password must be at least 6 characters' })
   password: string;
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { bootstrapApplication } from './app';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = 3110;
@@ -13,6 +14,16 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(PORT);
   Logger.debug(`Server running on ${await app.getUrl()}/api`);
