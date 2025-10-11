393c8876642435f952b01b545e436605471fd014
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-10-11 13:49:46 +0100

    removed jwt code

--- server/src/core/auth/auth.module.ts
+++ server/src/core/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User, UserSchema } from './schema/userSchema';
@@ -9,13 +8,6 @@ import { MailModule } from 'src/core/mail/mail.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-
-    // 👇 Add this block
-    JwtModule.register({
-      global: true, // optional — makes JwtService available app-wide
-      secret: process.env.JWT_RESET_SECRET || 'reset_secret_key',
-      signOptions: { expiresIn: '15m' },
-    }),
     MailModule,
   ],
   controllers: [AuthController],
